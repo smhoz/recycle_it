efc37f6ba8f9e5aed76eba5373473e861bd94ec7
Semih <semih2001aras@hotmail.com>
2022-02-12 18:35:13 +0300

    add theme in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:hackathon_app/core/init/notifier/provider_list.dart';
+import 'package:provider/provider.dart';
 
+import 'core/init/theme/app_theme.dart';
 import 'core/navigation/navigation_manager.gr.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MultiProvider(
+    providers: ProviderList.instance.providerItems,
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -14,6 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      theme: AppTheme.instance.theme,
       routerDelegate: AutoRouterDelegate(
         _appRouter,
         initialRoutes: [const SplashRoute()],
