1de99c04cb4a029c450768737e2de29e4be9f515
mstfmrt07 <60856457+mstfmrt07@users.noreply.github.com>
2022-02-12 14:23:38 +0100

    Spelling mistake fix

--- lib/view/pages/home_page/components/map_component/map_component.dart
+++ lib/view/pages/home_page/components/map_component/map_component.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class MapComponet extends StatelessWidget {
-  const MapComponet({Key? key}) : super(key: key);
+class MapComponent extends StatelessWidget {
+  const MapComponent({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
