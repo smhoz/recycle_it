308d4107ad0ee5d3442cecc62b9d2c9649f69a25
emreasik <asik.emre@outlook.com>
2022-02-12 19:55:05 +0100

    Const padding updated

--- lib/view/widgets/_widget_const.dart
+++ lib/view/widgets/_widget_const.dart
@@ -3,9 +3,6 @@ import 'package:flutter/material.dart';
 class WC {
   static const Widget tpDivider = Divider(color: Colors.transparent);
 
-  static Padding paddingAll({required Widget child}) {
-    const double paddingAllValue = 8.0;
-    return Padding(
-        padding: const EdgeInsets.all(paddingAllValue), child: child);
-  }
+  static Padding paddingAll({double? val, required Widget child})  => Padding(
+        padding:  EdgeInsets.all(val ?? 8), child: child);
 }
