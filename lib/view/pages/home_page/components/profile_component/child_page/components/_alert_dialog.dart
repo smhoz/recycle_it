4b12e37334ce459d01b5d9936a81f04e9569a133
Alp Can Marangoz <alpcanm@gmail.com>
2022-02-13 01:16:38 +0300

    created balance alert dialog class and static method

---
+++ lib/view/pages/home_page/components/profile_component/child_page/components/_alert_dialog.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../../core/utils/locator_get_it.dart';
+import '../cubit/balance_cubit.dart';
+
+class WalletAlertDialog {
+  static Future<void> showMyDialog(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                Text("Are you sure ?"),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Approve'),
+              onPressed: () {
+                getIt<BalanceCubit>().balanceUpdate();
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
