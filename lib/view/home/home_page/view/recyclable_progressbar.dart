a8b976906f09c86134709c6f6b17603a5837ec70
Semih <semih2001aras@hotmail.com>
2022-02-13 13:29:29 +0300

    add widget from percent_indicator in recyclable_progressbar

--- lib/view/home/home_page/view/recyclable_progressbar.dart
+++ lib/view/home/home_page/view/recyclable_progressbar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hackathon_app/core/extensions/context_extension.dart';
 import 'package:hackathon_app/core/model/recyclable_model.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class RecyclableProgressBar extends StatelessWidget {
   final Recyclable? recyclable;
@@ -7,21 +9,35 @@ class RecyclableProgressBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double percent = recyclable!.currentLoad! / recyclable!.maxLoad!;
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: Container(
-        height: 16,
-        width: 120,
-        decoration: BoxDecoration(
-          color: Colors.amber,
-          borderRadius: BorderRadius.circular(8.0),
-        ),
-        child: Column(
-          children: [
-            Text(recyclable!.type!.getName),
-            Text("${recyclable!.currentLoad} / ${recyclable!.maxLoad}"),
-          ],
-        ),
+      padding: context.paddingLow,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text(
+            recyclable!.type!.getName,
+            style: context.textTheme.bodyMedium!.copyWith(fontSize: 18),
+          ),
+          Padding(
+            padding: context.paddingHorizontalMedium,
+            child: _circularPercentIndicator(percent, context),
+          ),
+        ],
+      ),
+    );
+  }
+
+  CircularPercentIndicator _circularPercentIndicator(
+      double percent, BuildContext context) {
+    return CircularPercentIndicator(
+      radius: 28.0,
+      lineWidth: 4.0,
+      percent: percent,
+      progressColor: context.themeColor.secondary,
+      center: Text(
+        "%" + (percent * 100).toInt().toString(),
+        style: context.textTheme.bodyText1!.copyWith(fontSize: 14),
       ),
     );
   }
