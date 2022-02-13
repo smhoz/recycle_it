0c0c9d86eac4c1ff0e19b775bde79489fe1d2cf7
Semih <semih2001aras@hotmail.com>
2022-02-13 13:28:51 +0300

    Update profile_body and profile_form

--- lib/view/home/profile_page/view/profile_body.dart
+++ lib/view/home/profile_page/view/profile_body.dart
@@ -19,25 +19,20 @@ class ProfileBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            const Divider(height: 50),
-            const CircleAvatar(
-                child: FlutterLogo(
-                  size: 120,
-                ),
-                backgroundColor: Colors.transparent),
-            BlocBuilder<ProfileBloc, ProfileState>(
-              buildWhen: (previous, current) => previous != current,
-              builder: (context, state) {
-                return ProfileForm();
-              },
-            ),
-            const _WalletContainer(),
-            const _LogOutButton(),
-          ],
-        ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          const _LogOutButton(),
+          BlocBuilder<ProfileBloc, ProfileState>(
+            buildWhen: (previous, current) => previous != current,
+            builder: (context, state) {
+              return ProfileForm();
+            },
+          ),
+          const Spacer(),
+          const _WalletContainer(),
+        ],
       ),
     );
   }
@@ -56,10 +51,7 @@ class _WalletContainer extends StatelessWidget {
       margin: context.paddingLow,
       height: context.height * 0.10,
       child: ListTile(
-        leading: const Icon(
-          Icons.account_balance_wallet_outlined,
-          size: 32,
-        ),
+        leading: _navigateWalletIcon(),
         title: Text(
           "Cüzdanım",
           style: context.textTheme.headline1,
@@ -67,29 +59,44 @@ class _WalletContainer extends StatelessWidget {
         subtitle: BlocBuilder<ProfileBloc, ProfileState>(
           buildWhen: (previous, current) => previous != current,
           builder: (context, state) {
-            return Row(children: [
-              Icon(
-                Icons.circle,
-                color: CustomColors.instance.coinColor,
-                size: 24,
-              ),
-              SizedBox(
-                width: context.width * 0.02,
-              ),
-              Text(((getIt<GlobalRepository>().user?.balance) ?? 0).toString())
-            ]);
+            return _balanceRow(context);
           },
         ),
-        trailing: IconButton(
-            onPressed: (() {
-              context.router.pushNamed(RouteConsts.WALLET_PAGE);
-            }),
-            icon: const Icon(
-              Icons.arrow_forward_ios,
-              size: 24,
-            )),
+        trailing: _navigateIcon(context),
       ),
     );
+  }
+
+  Icon _navigateWalletIcon() {
+    return const Icon(
+      Icons.account_balance_wallet_outlined,
+      size: 32,
+    );
+  }
+
+  IconButton _navigateIcon(BuildContext context) {
+    return IconButton(
+        onPressed: (() {
+          context.router.pushNamed(RouteConsts.WALLET_PAGE);
+        }),
+        icon: const Icon(
+          Icons.arrow_forward_ios,
+          size: 24,
+        ));
+  }
+
+  Row _balanceRow(BuildContext context) {
+    return Row(children: [
+      Icon(
+        Icons.circle,
+        color: CustomColors.instance.coinColor,
+        size: 24,
+      ),
+      SizedBox(
+        width: context.width * 0.02,
+      ),
+      Text(((getIt<GlobalRepository>().user?.balance) ?? 0).toString())
+    ]);
   }
 }
 
@@ -102,13 +109,18 @@ class _LogOutButton extends StatelessWidget {
   const _LogOutButton({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return TextButton.icon(
-      onPressed: () => getIt<AuthRepository>().signOut().then(
-          (value) => context.router.replaceAll([const AuthControllerRoute()])),
-      icon: const Icon(
-        Icons.exit_to_app,
+    return Padding(
+      padding: context.paddingMedium,
+      child: TextButton.icon(
+        onPressed: () => getIt<AuthRepository>().signOut().then((value) =>
+            context.router.replaceAll([const AuthControllerRoute()])),
+        icon: const Icon(
+          Icons.exit_to_app,
+        ),
+        label: Text(
+          'Çıkış Yap',
+        ),
       ),
-      label: const Text('Log Out'),
     );
   }
 }
