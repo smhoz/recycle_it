0f834fb9057e6ac4013705b5a11d98dce4af14ef
Semih <semih2001aras@hotmail.com>
2022-02-13 16:46:40 +0300

    add isShowModalBottomSheet in home_view_model

--- lib/view/home/home_page/viewmodel/home_view_model.dart
+++ lib/view/home/home_page/viewmodel/home_view_model.dart
@@ -3,10 +3,19 @@ import 'package:flutter/cupertino.dart';
 class HomeViewModel extends ChangeNotifier {
   int _currentIndex = 0;
 
+  bool _isShowModalBottomSheet = false;
+
+  bool get isShowModalBottomSheet => _isShowModalBottomSheet;
+
   int get currentIndex => _currentIndex;
 
   void changeCurrentIndex(int index) {
     _currentIndex = index;
+    notifyListeners();
+  }
+
+  void changeIsShowModalBottomSheet(bool value) {
+    _isShowModalBottomSheet = value;
     notifyListeners();
   }
 }
